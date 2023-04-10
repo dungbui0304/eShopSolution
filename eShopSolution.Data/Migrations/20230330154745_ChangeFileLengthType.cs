bafb701bd810ace2207019493999c5b23114ccde
dungbui0304 <buithedung0304@gmail.com>
2023-04-10 11:21:16 +0700

    update

--- eShopSolution.Data/Migrations/20230330154745_ChangeFileLengthType.cs
+++ eShopSolution.Data/Migrations/20230330154745_ChangeFileLengthType.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -28,11 +27,6 @@ namespace eShopSolution.Data.Migrations
                 table: "ProductImages",
                 newName: "IX_ProductImages_ProductId");
 
-            migrationBuilder.AddColumn<Guid>(
-                name: "AppUserId",
-                table: "Transactions",
-                type: "uniqueidentifier",
-                nullable: true);
 
             migrationBuilder.AlterColumn<DateTime>(
                 name: "OrderDate",
@@ -43,18 +37,6 @@ namespace eShopSolution.Data.Migrations
                 oldClrType: typeof(DateTime),
                 oldType: "datetime2",
                 oldDefaultValue: new DateTime(2023, 3, 26, 22, 11, 2, 224, DateTimeKind.Local).AddTicks(5325));
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "AppUserId",
-                table: "Order",
-                type: "uniqueidentifier",
-                nullable: true);
-
-            migrationBuilder.AddColumn<Guid>(
-                name: "AppUserId",
-                table: "Carts",
-                type: "uniqueidentifier",
-                nullable: true);
 
             migrationBuilder.AlterColumn<string>(
                 name: "ImagePath",
@@ -79,84 +61,12 @@ namespace eShopSolution.Data.Migrations
                 table: "ProductImages",
                 column: "Id");
 
-            migrationBuilder.CreateTable(
-                name: "AppRoles",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    NormalizedName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AppRoles", x => x.Id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "AppUsers",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    FirstName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
-                    Dob = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    NormalizedUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    NormalizedEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
-                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
-                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
-                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
-                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
-                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_AppUsers", x => x.Id);
-                });
-
             migrationBuilder.UpdateData(
                 table: "Products",
                 keyColumn: "Id",
                 keyValue: 1,
                 column: "DateCreated",
                 value: new DateTime(2023, 3, 30, 22, 47, 44, 940, DateTimeKind.Local).AddTicks(7789));
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Transactions_AppUserId",
-                table: "Transactions",
-                column: "AppUserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Order_AppUserId",
-                table: "Order",
-                column: "AppUserId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Carts_AppUserId",
-                table: "Carts",
-                column: "AppUserId");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Carts_AppUsers_AppUserId",
-                table: "Carts",
-                column: "AppUserId",
-                principalTable: "AppUsers",
-                principalColumn: "Id");
-
-            migrationBuilder.AddForeignKey(
-                name: "FK_Order_AppUsers_AppUserId",
-                table: "Order",
-                column: "AppUserId",
-                principalTable: "AppUsers",
-                principalColumn: "Id");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_ProductImages_Products_ProductId",
@@ -166,32 +76,15 @@ namespace eShopSolution.Data.Migrations
                 principalColumn: "Id",
                 onDelete: ReferentialAction.Cascade);
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_Transactions_AppUsers_AppUserId",
-                table: "Transactions",
-                column: "AppUserId",
-                principalTable: "AppUsers",
-                principalColumn: "Id");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropForeignKey(
-                name: "FK_Carts_AppUsers_AppUserId",
-                table: "Carts");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_Order_AppUsers_AppUserId",
-                table: "Order");
 
             migrationBuilder.DropForeignKey(
                 name: "FK_ProductImages_Products_ProductId",
                 table: "ProductImages");
-
-            migrationBuilder.DropForeignKey(
-                name: "FK_Transactions_AppUsers_AppUserId",
-                table: "Transactions");
 
             migrationBuilder.DropTable(
                 name: "AppRoles");
@@ -199,33 +92,9 @@ namespace eShopSolution.Data.Migrations
             migrationBuilder.DropTable(
                 name: "AppUsers");
 
-            migrationBuilder.DropIndex(
-                name: "IX_Transactions_AppUserId",
-                table: "Transactions");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Order_AppUserId",
-                table: "Order");
-
-            migrationBuilder.DropIndex(
-                name: "IX_Carts_AppUserId",
-                table: "Carts");
-
             migrationBuilder.DropPrimaryKey(
                 name: "PK_ProductImages",
                 table: "ProductImages");
-
-            migrationBuilder.DropColumn(
-                name: "AppUserId",
-                table: "Transactions");
-
-            migrationBuilder.DropColumn(
-                name: "AppUserId",
-                table: "Order");
-
-            migrationBuilder.DropColumn(
-                name: "AppUserId",
-                table: "Carts");
 
             migrationBuilder.RenameTable(
                 name: "ProductImages",
