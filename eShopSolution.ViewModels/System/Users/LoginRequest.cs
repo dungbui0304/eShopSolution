54375b3c557735138387cce8ba25a060dce13cc9
dungbui0304 <buithedung0304@gmail.com>
2023-04-11 21:41:59 +0700

    Update login

---
+++ eShopSolution.ViewModels/System/Users/LoginRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace eShopSolution.ViewModels.System.Users
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Passwrod { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
