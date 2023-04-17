6af91b3f14639fc551018bcdc04efd5def760ab9
dungbui0304 <buithedung0304@gmail.com>
2023-04-17 16:55:24 +0700

    cookie authentication without identity

--- eShopSolution.Application/System/Users/UserService.cs
+++ eShopSolution.Application/System/Users/UserService.cs
@@ -21,7 +21,7 @@ namespace eShopSolution.Application.System
             _signInManager = signInManager;
             _roleManager = roleManager;
             _config = config;
-        }                                                                                                                               
+        }
         public async Task<string> Authenticate(LoginRequest request)
         {
             var user = await _userManager.FindByNameAsync(request.UserName);
@@ -35,7 +35,8 @@ namespace eShopSolution.Application.System
             {
                 new Claim(ClaimTypes.Email,user.Email),
                 new Claim(ClaimTypes.GivenName,user.FirstName),
-                new Claim(ClaimTypes.Role, string.Join(";",roles))
+                new Claim(ClaimTypes.Role, string.Join(";",roles)),
+                new Claim(ClaimTypes.Name, request.UserName)
                };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
